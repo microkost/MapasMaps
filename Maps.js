8d4401ec826bb66eb917ef104ca8f5d48a7334eb
JerryX <jerryx@darkruby.net>
2017-10-23 22:38:09 +0300

    marker string

--- Maps.js
+++ Maps.js
@@ -2,33 +2,18 @@
 var cordsWGS84 = { 'lat': 62.241642, 'lng': 25.759134 }; //google format when lat is first
 var zoomlevel = 13;
 var markerHeader = "";
-var contentString = '<div id="content">' +
-    '<div id="siteNotice">' +
-    '</div>' +
-    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-    '<div id="bodyContent">' +
-    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-    'sandstone rock formation in the southern part of the ' +
-    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
-    'south west of the nearest large town, Alice Springs; 450&#160;km ' +
-    '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
-    'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
-    'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
-    'Aboriginal people of the area. It has many springs, waterholes, ' +
-    'rock caves and ancient paintings. Uluru is listed as a World ' +
-    'Heritage Site.</p>' +
-    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-    'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
-    '(last visited June 22, 2009).</p>' +
-    '</div>' +
-    '</div>';
+var contentString = "";
 var markerFooter = "";
 
 //regular code start:
-function setMarkerOptions(header, body, footer) {
-    this.markerHeader = header;
-    this.contentString = body;
-    this.footer = footer;
+function setMarkerOptions(city, temperature, wind_speed) {
+    this.contentString = 
+    '<div>' +
+    '<h1>'+city+'</h1>' +
+    '<p><b>Temperature: </b>'+temperature+'</p>'+
+    '<p><b>Wind speed: </b>'+wind_speed+'</p>'+
+    '</div>';
+
 }
 
 function flushMarkerOptions() {
@@ -176,7 +161,6 @@ function getWeatherData(lat, lng)
     $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lng+"&appid="+apiKey+"&units=metric",function(data){
 
         city_name = data["name"];
-        country_name = data["sys"]["country"];
         weather_description = data["weather"][0]["description"];
         temp = data["main"]["temp"];
         pressure = data["main"]["pressure"];
